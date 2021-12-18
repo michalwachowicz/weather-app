0b5ab8568ac154dbe260d2d1df98178b8ae93349
Michał Wachowicz <wachowiczmichal1@gmail.com>
2021-12-18 13:42:35 +0100

    Configure the project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: './js/main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: './css/[name].css' }),
+    new ESLintPlugin(),
+  ],
+  mode: 'development',
+  devtool: 'inline-source-map',
+};
