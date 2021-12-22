11406f8bcbeb56036370e140b352a707f7ec5b01
Michał Wachowicz <wachowiczmichal1@gmail.com>
2021-12-22 11:24:58 +0100

    Add autoprefixer and build the project

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
       },
     ],
   },
