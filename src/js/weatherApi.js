f949451c21a8c1702007fcd134d75c2103588658
michalwachowicz <wachowiczmichal1@gmail.com>
2024-09-19 15:40:12 +0200

    Add hourly forecast to retrieved weather data

--- src/js/weatherApi.js
+++ src/js/weatherApi.js
@@ -17,6 +17,53 @@ const fetchData = async (searchQuery) => {
   }
 };
 
+const getHour = (datetime) => datetime.split(":")[0];
+const getMinutes = (datetime) => datetime.split(":")[1];
+
+const getHourlyForecast = ({ currentConditions, days }) => {
+  const { datetime, sunrise, sunset } = currentConditions;
+
+  const currentHour = parseInt(getHour(datetime), 10);
+  const sunriseHour = parseInt(getHour(sunrise), 10);
+  const sunsetHour = parseInt(getHour(sunset), 10);
+
+  let index = currentHour;
+  let sunriseAdded = false;
+  let sunsetAdded = false;
+
+  return new Array(27).fill("").map((_, i) => {
+    const currentDay = index > 23 ? 1 : 0;
+    const currentIndex = index % 24;
+    const hour = days[currentDay].hours[currentIndex];
+
+    if (!sunriseAdded && sunriseHour === currentIndex - 1) {
+      sunriseAdded = true;
+      return {
+        hour: `${getHour(sunrise)}:${getMinutes(sunrise)}`,
+        icon: "clear-day",
+        temperature: "Sunrise",
+      };
+    }
+
+    if (!sunsetAdded && sunsetHour === currentIndex - 1) {
+      sunsetAdded = true;
+      return {
+        hour: `${getHour(sunset)}:${getMinutes(sunset)}`,
+        icon: "clear-night",
+        temperature: "Sunset",
+      };
+    }
+
+    index += 1;
+
+    return {
+      hour: i === 0 ? "Now" : hour.datetime.split(":")[0],
+      icon: hour.icon,
+      temperature: hour.temp,
+    };
+  });
+};
+
 const getWeeklyForecast = ({ days }) =>
   days.map(({ datetime, tempmin, tempmax, icon }, index) => ({
     day: index === 0 ? "Today" : format(new Date(datetime), "EEEE"),
@@ -32,8 +79,7 @@ const getWeather = async (address) => {
   const { temp, conditions, humidity, cloudcover, windspeed } =
     data.currentConditions;
   const weekly = getWeeklyForecast(data);
-
-  // TODO: Hourly forecast (with sunset/sunrise)
+  const hourly = getHourlyForecast(data);
 
   return {
     address: data.resolvedAddress,
@@ -43,6 +89,7 @@ const getWeather = async (address) => {
     humidity,
     windspeed,
     weekly,
+    hourly,
   };
 };
 
